25d8dc1d1a3bcca659505e95b792b0f9ea3d93b0
Mike Carroll <mike@Mikes-MBP.attlocal.net>
2017-04-04 14:35:16 -0500

    make it pretty

--- numhosts.sh
+++ numhosts.sh
@@ -1,5 +1,13 @@
-NUMUSERS=`/opt/quest/bin/vastool -u host/ search -q "(&(uidNumber=*)(gidNumber=*)(unixHomeDirectory=*)(loginShell=*))" distinguishedName| wc -l`
-NUMSYS=`/opt/quest/bin/vastool -u host/ search -q '(&(objectCategory=Computer)(!(operatingSystem=Windows*)))' name | wc -l`
+#!/bin/bash
+
+# Determine the number of VAS enabled users and hosts are in AD
+# Note this will include systems that have been decommissioned without unjoining
+NUMUSERS=`/opt/quest/bin/vastool -u host/ \
+	search -q "(&(uidNumber=*)(gidNumber=*)(unixHomeDirectory=*)(loginShell=*))" distinguishedName \
+	| wc -l`
+NUMSYS=`/opt/quest/bin/vastool -u host/ \
+	search -q '(&(objectCategory=Computer)(!(operatingSystem=Windows*)))' name \
+	| wc -l`
 
 echo "Number of QAS Enabled Users:         $NUMUSERS"
 echo "Number of QAS Installed Unix Hosts:  $NUMSYS"
