a01494c2dcd9103478ff088182d8b1c70b832f4c
Mike Carroll <mike@Mikes-MBP.attlocal.net>
2017-04-04 14:47:21 -0500

    add root test

--- numhosts.sh
+++ numhosts.sh
@@ -2,12 +2,19 @@
 
 # Determine the number of VAS enabled users and hosts are in AD
 # Note this will include systems that have been decommissioned without unjoining
+
+# Test for root
+  if [[ $UID -ne 0 ]]; then
+    echo "$0 must be run as root"
+    exit 1
+  fi
+
 NUMUSERS=`/opt/quest/bin/vastool -u host/ \
-	search -q "(&(uidNumber=*)(gidNumber=*)(unixHomeDirectory=*)(loginShell=*))" distinguishedName \
-	| wc -l`
+  search -q "(&(uidNumber=*)(gidNumber=*)(unixHomeDirectory=*)(loginShell=*))" distinguishedName \
+  | wc -l`
 NUMSYS=`/opt/quest/bin/vastool -u host/ \
-	search -q '(&(objectCategory=Computer)(!(operatingSystem=Windows*)))' name \
-	| wc -l`
+  search -q '(&(objectCategory=Computer)(!(operatingSystem=Windows*)))' name \
+  | wc -l`
 
 echo "Number of QAS Enabled Users:         $NUMUSERS"
 echo "Number of QAS Installed Unix Hosts:  $NUMSYS"
