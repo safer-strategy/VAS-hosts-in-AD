f1eb6920364ab8c8d738100227bcd4fcd769b1ca
Mike Carroll <mike@Mikes-MBP.attlocal.net>
2017-04-10 20:04:25 -0500

    Better comments

--- pwdLastSet.sh
+++ pwdLastSet.sh
@@ -5,6 +5,18 @@
 # PasswordPolicy="120"
 PasswordPolicy="120"
 
+Test () {
+#: '  # Commented Out
+     echo "Hostname: $host"
+     echo "Todays Date: $todayDate"
+     echo "pwdLastSet Date: $lastUnix"
+     echo "Today - Last: $diffUnix"
+     echo "Days from Last Change:: $diffdays"
+     echo "Remaining: $daysremaining"
+     echo "Expired days: $daysexpired"
+#'
+}
+
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
@@ -14,9 +26,11 @@ todayDate=`date "+%s"`
 echo ""
 
 for host in `
+  # Extract a list of non-windows Computer accounts
   /opt/quest/bin/vastool -u host/ search '(&(objectclass=computer)(!(operatingSystem=Windows*)))' dn \
   | awk -F = '{print $2}'| awk -F , '{print $1}'`
   do
+    # Locate the time the computer account changed
     pwdLastSet=`/opt/quest/bin/vastool -u host/ search "(&(objectclass=computer)(sAMAccountName=${host}*))" pwdLastSet \
     | grep pwdLastSet | awk '{print $2}'`
     # Convert value to pwdLastSet to Unix
@@ -27,22 +41,18 @@ for host in `
     daysexpired=$((( $diffdays - $PasswordPolicy )))
     daysremaining=$((( $PasswordPolicy - $diffdays )))
 
-: '  # Commented Out
-     echo "Hostname: $host"
-     echo "Todays Date: $todayDate"
-     echo "pwdLastSet Date: $lastUnix"
-     echo "Today - Last: $diffUnix"
-     echo "Days from Last Change:: $diffdays"
-     echo "Remaining: $daysremaining"
-     echo "Expired days: $daysexpired"
-'
+    : '
+    Test # Uncomment to run Test function
+    '
 
     if [[ "$PasswordPolicy" -gt "$diffdays"  ]]
       then
-#        echo "$host Password valid: $daysremaining"
-#        echo ""
+        # Host has a valid Computer password.
+        # echo "$host Password valid: $daysremaining"
+        # echo ""
         continue
       else
+          # Host has expired Computer account password
           echo "$host invalid: Password expired  $daysexpired"
           echo ""
         continue
